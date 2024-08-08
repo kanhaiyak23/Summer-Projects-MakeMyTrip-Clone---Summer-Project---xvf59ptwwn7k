6666139d7501d1340e81cd3472dbab88839676c2
kanhaiyak23 <kanhaiyakumarsingh086@gmail.com>
2024-08-08 17:49:15 +0530

    a

--- src/flight/flight_search_bar.js
+++ src/flight/flight_search_bar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
 const FlightSearchBar = () => {
   const [airports, setAirports] = useState([]);
